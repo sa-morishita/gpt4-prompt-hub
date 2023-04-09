3e4ff8975d5a381d9e9c3b5c7c0cf2ec178d2ea9
Satoshi Morishita <morishita@q-3.jp>
2023-04-09 15:24:06 +0900

    TagSearchのloadingを復元

--- src/components/TagSearch/index.tsx
+++ src/components/TagSearch/index.tsx
@@ -47,7 +47,11 @@ const TagSearch: FC<Props> = ({ tags }) => {
           </button>
         ))}
       </div>
-
+      {getPromptsByTag.isFetching && getPromptsByTag.isLoading && (
+        <div className="text-center py-6">
+          <span className="relative inset-0 inline-flex h-6 w-6 animate-spin items-center justify-center rounded-full border-2 border-emerald-600 after:absolute after:h-8 after:w-8 after:rounded-full after:border-2 after:border-y-purple-600 after:border-x-transparent"></span>
+        </div>
+      )}
       {getPromptsByTag.isSuccess && (
         <PromptCards prompts={getPromptsByTag.data} />
       )}
